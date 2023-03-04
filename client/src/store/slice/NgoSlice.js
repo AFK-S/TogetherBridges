7e826f29b814c208d65a5ca909e93a26b834c177
Karan2310 <karandeep9619@gmail.com>
2023-03-04 16:24:49 +0530

    NGO card id connected with url

--- client/src/store/slice/NgoSlice.js
+++ client/src/store/slice/NgoSlice.js
@@ -4,8 +4,16 @@ const NgoSlice = createSlice({
     name: "ngo",
     initialState: [
         {
-            "name": "NGO 1",
+            "name": "NGO 0",
             "address": "Address 1",
+            "phone": "1234567890",
+            "id": 0,
+            "description":
+                "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum doloremque officiis assumenda! Esse quasi tenetur perspiciatis sint quas animi, aut ratione, blanditiis aperiam dicta unde nesciunt. Adipisci culpa dolores nihil?,",
+        },
+        {
+            "name": "NGO 1",
+            "address": "Address 2",
             "phone": "1234567890",
             "id": 1,
             "description":
@@ -13,17 +21,9 @@ const NgoSlice = createSlice({
         },
         {
             "name": "NGO 2",
-            "address": "Address 2",
-            "phone": "1234567890",
-            "id": 2,
-            "description":
-                "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum doloremque officiis assumenda! Esse quasi tenetur perspiciatis sint quas animi, aut ratione, blanditiis aperiam dicta unde nesciunt. Adipisci culpa dolores nihil?,",
-        },
-        {
-            "name": "NGO 3",
             "address": "Address 3",
             "phone": "1234567890",
-            "id": 3,
+            "id": 2,
             "description":
                 "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum doloremque officiis assumenda! Esse quasi tenetur perspiciatis sint quas animi, aut ratione, blanditiis aperiam dicta unde nesciunt. Adipisci culpa dolores nihil?,",
         }
