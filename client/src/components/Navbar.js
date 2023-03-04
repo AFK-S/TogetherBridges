6b62c6b64e92620501cd8b3c478ec58ded9496a3
Karan2310 <karandeep9619@gmail.com>
2023-03-04 22:57:21 +0530

    Event tabs added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -61,7 +61,7 @@ const Navbar = () => {
             </li>
             <li>
               <NavLink
-                to="/about"
+                to="/events"
                 href="#"
                 className={({ isActive }) =>
                   isActive
@@ -69,10 +69,10 @@ const Navbar = () => {
                     : "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 }
               >
-                About
+                Events
               </NavLink>
             </li>
-            <li>
+            {/* <li>
               <NavLink
                 to="/contact"
                 href="#"
@@ -85,7 +85,7 @@ const Navbar = () => {
               >
                 Contact
               </NavLink>
-            </li>
+            </li> */}
             <li>
               <NavLink
                 to="/ngo"
