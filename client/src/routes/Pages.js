b54aeaaddd0d59694e1d88474cc8be07f83f53f3
Karan2310 <karandeep9619@gmail.com>
2023-03-04 16:15:17 +0530

    NGO list public

--- client/src/routes/Pages.js
+++ client/src/routes/Pages.js
@@ -4,6 +4,7 @@ import Home from '../pages/Home'
 import About from '../pages/About'
 import { Routes, Route } from 'react-router-dom'
 import Ngo from '../pages/Ngo'
+import NGOcard from '../components/NGOcard'
 
 const Pages = () => {
     return (
@@ -11,6 +12,7 @@ const Pages = () => {
             <Navbar />
             <Routes>
                 <Route path="/" index element={<Home />} />
+                <Route path="/ngo" index element={<NGOcard />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/ngo/:id" element={<Ngo />} />
                 <Route path="*" element={<h1 className='text-3xl font-bold text-center my-80'>404 Not Found</h1>} />
