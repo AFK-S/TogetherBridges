2840952be80f1a3f5eaae9b912f2ede18ba2ecb5
Karan2310 <karandeep9619@gmail.com>
2023-03-04 23:16:19 +0530

    Navbar active tab updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -53,7 +53,7 @@ const Navbar = () => {
                 aria-current="page"
                 className={({ isActive }) =>
                   isActive
-                    ? "active"
+                    ? "text-blue-700 dark:text-blue-300"
                     : "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 }
               >
@@ -66,7 +66,7 @@ const Navbar = () => {
                 href="#"
                 className={({ isActive }) =>
                   isActive
-                    ? "active"
+                    ? "text-blue-700 dark:text-blue-300"
                     : "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 }
               >
@@ -80,7 +80,7 @@ const Navbar = () => {
                 aria-current="page"
                 className={({ isActive }) =>
                   isActive
-                    ? "active"
+                    ? "text-blue-700 dark:text-blue-300"
                     : "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 }
               >
