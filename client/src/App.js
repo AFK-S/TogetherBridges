3ad214cf5ca6705d8f6cd6b4cef63e6ddf62accb
FaizanPotrick <faizan.potrick02@gmail.com>
2023-03-05 05:20:58 +0530

    Add useContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,55 @@
-import Auth from "./routes/Auth";
-import Pages from "./routes/Pages";
-import { useSelector } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Routes } from "react-router-dom";
-import { useEffect } from "react";
-import { useCookies } from "react-cookie";
-import { useDispatch } from "react-redux";
-import { login } from "./store/slice/IsLoggedInSlice";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import React, { useContext } from "react";
+import { StateContext } from "./context/StateContext";
+import Navbar from "./components/Navbar";
+import Landing from "./pages/Landing";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register/NGO";
+import NGOs from "./pages/NGOs";
+import NGO from "./pages/NGO";
+import Events from "./pages/Events";
+import EditProfile from "./pages/EditProfile";
+import Loading from "./components/Loading";
+import Alert from "./components/Alert";
 
 function App() {
-  const loginStatus = useSelector((state) => state.isLoggedIn);
-  const dispatch = useDispatch();
-  const [cookies] = useCookies(["user_id"]);
-
-  useEffect(() => {
-    if (cookies.user_id) {
-      console.log("cookies", cookies);
-      dispatch(login());
-    }
-  }, []);
-
+  const { isLogin } = useContext(StateContext);
   return (
     <>
       <Router>
-        <Routes>
-          {/* <Route path="/ngo" element={<Ngo />} /> */}
-          {/* <Route path="/ngo/:id" element={<Ngo />} /> */}
-        </Routes>
-        {loginStatus ? <Pages /> : <Auth />}
+        {isLogin ? (
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/" index element={<Dashboard />} />
+              <Route path="/login" element={<Navigate to="/" />} />
+              <Route path="/register" element={<Navigate to="/" />} />
+              <Route path="/ngos" element={<NGOs />} />
+              <Route path="/ngos/:ngo_id" element={<NGO />} />
+              <Route path="/events" element={<Events />} />
+              <Route path="/editprofile" element={<EditProfile />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </>
+        ) : (
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/ngos" element={<NGOs />} />
+            <Route path="/ngos/:ngo_id" element={<NGO />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        )}
       </Router>
+      <Loading />
+      <Alert />
     </>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
+import StateProvider from "./context/StateContext";
+import { CookiesProvider } from "react-cookie";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import { CookiesProvider } from "react-cookie";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <CookiesProvider>
-      <App />
+      <StateProvider>
+        <App />
+      </StateProvider>
     </CookiesProvider>
   </Provider>
 );
