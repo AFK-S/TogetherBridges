92639e8f3d471cdba1c1f05fdc3f8352cde235a1
FaizanPotrick <faizan.potrick02@gmail.com>
2023-03-04 16:04:45 +0530

    Connect Login and Register

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,41 +1,53 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { login } from "../store/slice/IsLoggedInSlice";
+// import { useDispatch } from "react-redux";
+// import { login } from "../store/slice/IsLoggedInSlice";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./Login.css";
 
 const Login = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const loginBtn = () => {
-    dispatch(login());
-  };
-  const initialInput = { email: "", password: "" };
-  const [inputFields, setInputFields] = useState(initialInput);
-  const [showPassword, setShowPassword] = useState(false);
+  // const loginBtn = () => {
+  //   dispatch(login());
+  // };
+
   const [isLoading, setIsLoading] = useState(false);
+  const [login, setLogin] = useState({
+    email_address: "",
+    password: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
 
-  const handleChange = (e) => {
+  const onChange = (e) => {
     const { name, value } = e.target;
-    setInputFields({ ...inputFields, [name]: value });
+    setLogin({ ...login, [name]: value });
   };
 
-  const handleSubmit = (e) => {
-    
-  };
-
-  const togglePassword = () => {
-    setShowPassword(!showPassword);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const { data } = await axios.post("/api/login", login);
+      console.log(data);
+      setLogin({
+        email_address: "",
+        password: "",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
   };
 
   return (
-    <div className="login container mx-auto sm:px-4 max-w-full p-6">
+    <div className="login bg-[#909090] flex justify-center items-center container mx-auto sm:px-4 max-w-full p-6 h-screen">
       <div className="container mx-auto sm:px-0">
         <div className="flex flex-wrap ">
           <div className="sm:w-full pr-4 pl-4 md:w-1/2 flex items-center justify-center flex-col p-6 md:p-12">
             <form
               className="text-start flex flex-col w-full"
-              onSubmit={handleSubmit}
+              onSubmit={onSubmit}
             >
               <div className="text-start w-full">
                 <h3 className="flex items-center text-start mb-5 font-bold">
@@ -55,21 +67,26 @@ const Login = () => {
                 Welcome Back!
               </p>
               <div className="flex flex-col mb-4">
-                <label htmlFor="Email">Email</label>
+                <label htmlFor="Email">Email Address</label>
                 <input
                   type="email"
-                  name="email"
-                  onChange={handleChange}
-                  value={inputFields.email}
+                  name="email_address"
+                  onChange={onChange}
+                  value={login.email_address}
+                  className="border-2 border-gray-500 py-1.5 px-2 rounded-lg"
+                  required
                 />
               </div>
-              <div className="flex flex-col relative">
+              <div className="flex flex-col relative font-medium">
                 <label htmlFor="Password">Password</label>
                 <input
                   type={showPassword === true ? "text" : "password"}
                   name="password"
-                  onChange={handleChange}
-                  value={inputFields.password}
+                  onChange={onChange}
+                  value={login.password}
+                  className="border-2 border-gray-500 py-1.5 px-2 rounded-lg"
+                  autoComplete="off"
+                  required
                 />
                 {showPassword === true ? (
                   <svg
@@ -77,8 +94,8 @@ const Login = () => {
                     stroke="currentColor"
                     strokeWidth="1.5"
                     viewBox="0 0 24 24"
-                    className="absolute right-4 top-[38px] h-5 w-5 text-gray-400 cursor-pointer"
-                    onClick={togglePassword}
+                    className="absolute right-4 top-[38px] h-5 w-5 text-gray-600 cursor-pointer"
+                    onClick={() => setShowPassword(!showPassword)}
                   >
                     <path d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path>
                     <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
@@ -89,52 +106,71 @@ const Login = () => {
                     stroke="currentColor"
                     strokeWidth="1.5"
                     viewBox="0 0 24 24"
-                    className="absolute right-4 top-[38px] h-5 w-5 text-gray-400 cursor-pointer"
-                    onClick={togglePassword}
+                    className="absolute right-4 top-[38px] h-5 w-5 text-gray-600 cursor-pointer"
+                    onClick={() => setShowPassword(!showPassword)}
                   >
                     <path d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"></path>
                   </svg>
                 )}
               </div>
-              <p className="password-warning m-0">
+              <div className="text-[#109ee6] font-medium text-sm">
                 Password must include atleast one uppercase, lowecase, number
                 and symbols like [@ ! $ ? % &]
-              </p>
-              <Link to="/reset_password" id="fpassword">
+              </div>
+              <Link
+                to="/reset_password"
+                className="text-gray-500 font-medium mt-1 text-sm hover:text-gray-700"
+              >
                 Forgot password ?
               </Link>
-              <button className="mt-5 flex justify-center items-center">
-                {isLoading === true ? (
-                  <div className="spinner-border text-gray-100" role="status">
-                    <span clasName="visually-hidden"></span>
-                  </div>
+              <button
+                className="mt-5 flex justify-center items-center"
+                type="submit"
+              >
+                {isLoading ? (
+                  <svg
+                    className="w-6 h-6 text-transparent animate-spin fill-white"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
                 ) : (
-                  <>
+                  <div className="text-white flex justify-center items-center">
                     Login
                     <svg
                       fill="none"
                       stroke="currentColor"
                       strokeWidth="2"
                       viewBox="0 0 24 24"
-                      className="w-5 h-5 ml-1"
+                      className="w-5 h-5 ml-0.5"
                     >
                       <path d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path>
                     </svg>
-                  </>
+                  </div>
                 )}
               </button>
             </form>
-            <div className="register-btn w-full mt-3 text-center">
-              <p>
-                New to PyschHeal ?{" "}
-                <span>
-                  <Link to="/register">Register Here</Link>{" "}
-                </span>
-              </p>
+            <div className="text-sm font-medium mt-2 text-center">
+              New to PyschHeal?
+              <Link to="/register" className="text-[#109ee6] ml-1">
+                Register Here
+              </Link>{" "}
             </div>
           </div>
           <div className="vector-div hidden md:flex md:w-1/2 overflow-hidden">
-            <img src="loginImg.jpg" alt="Be Happy" className='max-w-full h-auto' />
+            <img
+              src="loginImg.jpg"
+              alt="Be Happy"
+              className="max-w-full h-auto"
+            />
           </div>
         </div>
       </div>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,216 +1,141 @@
-import React from 'react'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import validator from 'validator'
-import "./Register.css"
-
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Register.css";
+import axios from "axios";
 
 const Register = () => {
-  const initialInput = { first_name: "", last_name: "", email: "", password: "", cPassword: "", mobile_no: "", dob: "", gender: "" }
-  const [inputFields, setInputFields] = useState(initialInput)
-  const [password, setPassword] = useState(false)
-  const [cPassword, setCpassword] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [register, setRegister] = useState({
+    name: "",
+    email_address: "",
+    phone_number: "",
+    gender: "",
+    interested_ngo: "",
+  });
 
-  const handleChange = (e) => {
-      const { name, value } = e.target;
-      setInputFields({ ...inputFields, [name]: value })
-  }
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setRegister({ ...register, [name]: value });
+  };
 
-  const handleSubmit = (e) => {
-      e.preventDefault();
-      var fname = inputFields.first_name
-      var lname = inputFields.last_name
-      var gender = inputFields.gender
-      var dob = inputFields.dob
-      var email = inputFields.email
-      var password = inputFields.password
-      var cPassword = inputFields.cPassword
-      var mobile_no = inputFields.mobile_no
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const { data } = await axios.post("/api/register/volunteer", register);
+      console.log(data);
+      setRegister({
+        name: "",
+        email_address: "",
+        phone_number: "",
+        gender: "",
+        interested_ngo: "",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
 
-      if (validator.isEmpty(fname.trim()) === true || validator.isLength(fname, 3) === false) {
-          alert("Enter valid First Name")
-      } else if (validator.isEmpty(lname.trim()) === true || validator.isLength(lname, 3) === false) {
-          alert("Enter valid Last Name")
-      } else if (validator.isEmpty(gender) === true) {
-          alert("Select Gender")
-      } else if (validator.isDate(dob) === false) {
-          alert("Enter valid Date of Birth")
-      } else if (validator.isInt(mobile_no) === false) {
-          alert("Enter valid Mobile Number")
-      } else if (validator.isEmail(email) === false) {
-          alert("Enter valid Email")
-      } else if (validator.isStrongPassword(password) === false) {
-          alert("Password must include atleast one uppercase, lowecase, number and symbols like [@ ! $ ? % &]")
-      } else if (validator.equals(password, cPassword) === false) {
-          console.log(validator.equals(password, cPassword));
-          alert("Passwords do not match")
-      } else {
-          // setIsLoading(true);
-          // axios.post(`${baseUrl}/api/signup/`, inputFields)
-          //     .then((res) => {
-          //         console.log(res);
-          //         alert("Registered Successfully!")
-          //         setIsLoading(false);
-          //         setInputFields(initialInput)
-          //         window.location.href = "/";
-          //     })
-          //     .catch((error) => {
-          //         setIsLoading(false);
-          //         console.log(error);
-          //         if (error.response) {
-          //             if (error.response.status === 406) {
-          //                 alert("user aleady exist")
-          //                 window.location.href = "/"
-          //             } else {
-          //                 alert("Something went wrong!")
-          //             }
-          //         }
-          //     })
-      }
-  }
-
-  const showPassword = () => {
-      setPassword(!password)
-  }
-  const showCpassword = () => {
-      setCpassword(!cPassword)
-  }
   return (
     <div className="register container sm:px-4 max-w-full mx-auto p-6">
-    <div className="container mx-auto sm:px-4">
-        <form onSubmit={handleSubmit}>
-            <div className="flex flex-wrap items-center justify-center p-6 md:p-12">
+      <div className="container mx-auto sm:px-4">
+        <form onSubmit={onSubmit}>
+          <div className="flex flex-wrap items-center justify-center p-6 md:p-12">
             <div className="text-start w-full">
-                <h3 className="flex items-center text-start mb-5 font-bold">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    viewBox="0 0 24 24"
-                    className="w-9 h-9 mr-2"
-                  >
-                    <path d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"></path>
-                  </svg>
-                  PyschHeal
-                </h3>
-              </div>
-                
-                <div className="md:w-1/2 pr-4 pl-4 p-0 md:px-12">
-                    <div className="flex flex-col input-fields">
-                        <label>First Name</label>
-                        <input onChange={handleChange} type="text" value={inputFields.first_name} name="first_name" />
-                    </div>
-                    <div className="flex flex-col input-fields">
-                        <label>Last Name</label>
-                        <input onChange={handleChange} type="text" value={inputFields.last_name} name="last_name" />
-                    </div>
-
-
-                    <div className="flex flex-col input-fields">
-                        <label>Gender</label>
-                        <div className="flex">
-                            <div className="flex items-center">
-                                <input onChange={handleChange} type="radio" name="gender" id="Male" value="male" />
-                                <label>Male</label>
-                            </div>
-                            <div className="flex items-center ms-4">
-                                <input onChange={handleChange} type="radio" name="gender" id="Female" value="female" />
-                                <label>Female</label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-col input-fields">
-                        <label>Date of Birth</label>
-                        <input onChange={handleChange} type="date" value={inputFields.dob} name="dob" />
-                    </div>
-
-                </div>
-
-                <div className="md:w-1/2 pr-4 pl-4 p-0 md:px-12">
-
-                    <div className="flex flex-col input-fields">
-                        <label>Mobile Number</label>
-                        <input onChange={handleChange} type="number" value={inputFields.mobile_no} name="mobile_no" />
-                    </div>
-
-                    <div className="flex flex-col input-fields">
-                        <label>Email ID</label>
-                        <input onChange={handleChange} type="email" value={inputFields.email} name="email" />
-                    </div>
-
-                    <div className="flex flex-col input-fields relative">
-                        <label>Password</label>
-                        <input onChange={handleChange} type={password === true ? "text" : "password"} value={inputFields.password} name="password" />
-                        {password === true ? (
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    viewBox="0 0 24 24"
-                    className="absolute right-4 top-[38px] h-5 w-5 text-gray-400 cursor-pointer"
-                    onClick={showPassword}
-                  >
-                    <path d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path>
-                    <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                  </svg>
-                ) : (
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    viewBox="0 0 24 24"
-                    className="absolute right-4 top-[38px] h-5 w-5 text-gray-400 cursor-pointer"
-                    onClick={showPassword}
-                  >
-                    <path d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"></path>
-                  </svg>
-                )}
-                    </div>
-
-                    <div className="flex flex-col input-fields relative">
-                        <label>Confirm Password</label>
-                        <input onChange={handleChange} type={cPassword === true ? "text" : "password"} value={inputFields.cPassword} name="cPassword" />
-                        {cPassword === true ? (
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    viewBox="0 0 24 24"
-                    className="absolute right-4 top-[38px] h-5 w-5 text-gray-400 cursor-pointer"
-                    onClick={showCpassword}
-                  >
-                    <path d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path>
-                    <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                  </svg>
-                ) : (
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    viewBox="0 0 24 24"
-                    className="absolute right-4 top-[38px] h-5 w-5 text-gray-400 cursor-pointer"
-                    onClick={showCpassword}
-                  >
-                    <path d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"></path>
-                  </svg>
-                )}
-                    </div>
-                </div>
-                <button className='mt-4 flex justify-center items-center'>
-                    {isLoading === true ? <div className="spinner-border text-gray-100" role="status">
-                        <span className="visually-hidden">Loading...</span>
-                    </div> : "Register"}
-                </button>
-                <div className="register-btn w-full mt-3 text-center">
-                    <p>You are already registered ? <span><Link to="/Login">Back to Login</Link> </span></p>
-                </div>
+              <h3 className="flex items-center text-start mb-5 font-bold">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  viewBox="0 0 24 24"
+                  className="w-9 h-9 mr-2"
+                >
+                  <path d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"></path>
+                </svg>
+                PyschHeal
+              </h3>
             </div>
+            <div className="w-full pr-4 pl-4 p-0 md:px-12">
+              <div className="flex flex-col input-fields">
+                <label>Name</label>
+                <input
+                  onChange={onChange}
+                  type="text"
+                  name="name"
+                  value={register.name}
+                  required
+                />
+              </div>
+              <div className="flex flex-col input-fields">
+                <label>Email Address</label>
+                <input
+                  onChange={onChange}
+                  type="email"
+                  name="email_address"
+                  value={register.email_address}
+                  required
+                />
+              </div>
+              <div className="flex flex-col input-fields">
+                <label>Phone Number</label>
+                <input
+                  onChange={onChange}
+                  type="text"
+                  value={register.phone_no}
+                  name="phone_no"
+                  required
+                />
+              </div>
+              <div className="flex flex-col input-fields">
+                <label>Gender</label>
+                <div className="flex">
+                  <div className="flex items-center">
+                    <input
+                      onChange={onChange}
+                      type="radio"
+                      name="gender"
+                      value="male"
+                      required
+                    />
+                    <label>Male</label>
+                  </div>
+                  <div className="flex items-center ml-4">
+                    <input
+                      onChange={onChange}
+                      type="radio"
+                      name="gender"
+                      value="female"
+                      required
+                    />
+                    <label>Female</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button className="mt-4 flex justify-center items-center">
+              {isLoading === true ? (
+                <div className="spinner-border text-gray-100" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              ) : (
+                "Register"
+              )}
+            </button>
+            <div className="register-btn w-full mt-3 text-center">
+              <p>
+                You are already registered ?{" "}
+                <span>
+                  <Link to="/Login">Back to Login</Link>{" "}
+                </span>
+              </p>
+            </div>
+          </div>
         </form>
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
