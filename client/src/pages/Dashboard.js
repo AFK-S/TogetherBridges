4e17c4afbe864aaafbed2038191954e0b18de871
Karan2310 <karandeep9619@gmail.com>
2023-03-05 01:22:05 +0530

    Add Announcement added

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -4,18 +4,31 @@ import { NavLink } from "react-router-dom";
 import { logout } from "../store/slice/IsLoggedInSlice";
 import axios from "axios";
 import { addEvent } from "../store/slice/EventsSlice";
+import { addAnnouncement } from "../store/slice/EventsSlice";
 
 const Dashboard = () => {
   const user = useSelector((state) => state.UserSlice);
+  const dispatch = useDispatch();
   const initialData = { name: "", description: "", date: "" };
   const [eventData, setEventData] = useState(initialData);
-  const dispatch = useDispatch();
-
   const [modal, setModal] = useState(false);
+  const [modal2, setModal2] = useState(false);
+  const [announcement, setAnnouncement] = useState("");
 
   const logoutBtn = () => {
     axios.get("/api/logout");
     dispatch(logout());
+  };
+
+  const onChangeAnnounce = (e) => {
+    setAnnouncement(e.target.value);
+  };
+
+  const addAnnouncementBtn = (e) => {
+    e.preventDefault();
+    dispatch(addAnnouncement({ title: announcement }));
+    setAnnouncement("");
+    setModal2(false);
   };
 
   const handleChange = (e) => {
@@ -80,9 +93,12 @@ const Dashboard = () => {
           <h1>Edit events</h1>
           <i className="fa-solid fa-pen-to-square ml-3"></i>
         </NavLink>
-        <div className="w-full bg-gray-200 hover:bg-gray-300 cursor-pointer p-5 rounded-xl flex justify-center items-center">
-          <h1>Edit Announcement</h1>
-          <i className="fa-solid fa-pen-to-square ml-3"></i>
+        <div
+          className="w-full bg-gray-200 hover:bg-gray-300 cursor-pointer p-5 rounded-xl flex justify-center items-center"
+          onClick={() => setModal2(true)}
+        >
+          <h1>Add Announcement</h1>
+          <i className="fa-solid fa-plus ml-3"></i>
         </div>
       </div>
       {/* <!-- Modal content --> */}
@@ -172,6 +188,62 @@ const Dashboard = () => {
                   </div>
                   <button
                     onClick={addEventBtn}
+                    className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Submit
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Modal 2 */}
+      <div
+        className={`modal absolute top-0 left-0 w-full h-full flex items-center justify-center ${
+          modal2 ? "block" : "hidden"
+        }`}
+        style={{ backgroundColor: "rgba(0, 1, 1, 0.3)" }}
+      >
+        <div
+          aria-hidden="true"
+          className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full"
+        >
+          <div className="relative w-full h-full max-w-md md:h-auto mx-auto">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <button
+                onClick={() => setModal2(false)}
+                type="button"
+                className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                data-modal-hide="authentication-modal"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path>
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+              <div className="px-6 py-6 lg:px-8">
+                <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+                  Add a Announcements
+                </h3>
+                <form className="space-y-6">
+                  <textarea
+                    value={announcement}
+                    onChange={onChangeAnnounce}
+                    rows="4"
+                    name="announcement"
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Type your announcement here.."
+                  ></textarea>
+                  <button
+                    onClick={addAnnouncementBtn}
                     className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
                     Submit
